42464aa05a666425181f17975f69919f712e218e
Sential <sentialx@gmail.com>
2019-02-23 01:00:11 +0100

    Add windows attaching

--- src/main/app-window.ts
+++ src/main/app-window.ts
@@ -2,8 +2,14 @@ import { BrowserWindow, app } from 'electron';
 import { resolve, join } from 'path';
 import { platform } from 'os';
 import mouseEvents from 'mouse-hooks';
+import { windowManager, Window } from 'node-window-manager';
+import console = require('console');
 
 export class AppWindow extends BrowserWindow {
+  public windows: Window[] = [];
+  public selectedWindow: Window;
+  public isMoving = false;
+
   constructor() {
     super({
       frame: process.env.ENV === 'dev' || platform() === 'darwin',
@@ -31,8 +37,46 @@ export class AppWindow extends BrowserWindow {
       this.loadURL(join('file://', app.getAppPath(), 'build/app.html'));
     }
 
-    mouseEvents.on('mouse-up', () => {
-      
+    this.on('move', () => {
+      if (this.selectedWindow) {
+        this.selectedWindow.setBounds(this.getContentArea());
+      }
     });
+
+    const handle = this.getNativeWindowHandle().readInt32LE(0);
+    const currentWindow = new Window(handle);
+
+    mouseEvents.on('mouse-up', () => {
+      const window = windowManager.getActiveWindow();
+      const contentArea = this.getContentArea();
+      const bounds = window.getBounds();
+
+      if (
+        !this.windows.find(x => x.handle === window.handle) &&
+        window.handle !== handle &&
+        bounds.x >= contentArea.x &&
+        bounds.x <= contentArea.x + contentArea.width &&
+        bounds.y >= contentArea.y - 42 &&
+        bounds.y <= contentArea.y
+      ) {
+        window.setParent(currentWindow);
+
+        this.selectedWindow = window;
+        this.windows.push(window);
+
+        setTimeout(() => {
+          window.setBounds(contentArea);
+        }, 50);
+      }
+    });
+  }
+
+  getContentArea() {
+    const bounds = this.getContentBounds();
+
+    bounds.y += 42;
+    bounds.height -= 42;
+
+    return bounds;
   }
 }

6b2563e810c6bc310ea59e134ed6cad7ca526bd7
Sential <sentialx@gmail.com>
2019-06-17 19:38:40 +0200

    fix: show devtools only in dev mode

--- src/main/app-window.ts
+++ src/main/app-window.ts
@@ -60,9 +60,9 @@ export class AppWindow extends BrowserWindow {
       console.error(error);
     });
 
-    this.webContents.openDevTools({ mode: 'detach' });
-
     if (process.env.ENV === 'dev') {
+      this.webContents.openDevTools({ mode: 'detach' });
+
       this.loadURL('http://localhost:4444/app.html');
     } else {
       this.loadURL(join('file://', app.getAppPath(), 'build/app.html'));

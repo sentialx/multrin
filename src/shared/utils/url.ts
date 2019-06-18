fee2103ac23da10ca0a6c90edf69135848a6f829
Hongarc <aqua.hong.97@gmail.com>
2019-06-19 01:58:37 +0700

    refactor: simple check url

--- src/shared/utils/url.ts
+++ src/shared/utils/url.ts
@@ -1,8 +1,5 @@
-export const isURL = (input: string): boolean => {
-  const pattern = /^(?:\w+:)?\/\/([^\s.]+\.\S{2}|localhost[:?\d]*)\S*$/;
+const pattern = /^(\w+:\/\/)?([^\s.]+\.\S{2}|localhost[:?\d]*)\S*$/;
 
-  if (pattern.test(input)) {
-    return true;
-  }
-  return pattern.test(`http://${input}`);
+export const isURL = (input: string): boolean => {
+  return pattern.test(input);
 };

53002c96ff12dc1c0bf9e9bdfc2765e9fa84a567
Eryk Rakowski <sentialx@gmail.com>
2019-06-26 13:25:45 +0200

    chore: delete url.ts

--- src/shared/utils/url.ts
+++
@@ -1,8 +0,0 @@
-export const isURL = (input: string): boolean => {
-  const pattern = /^(?:\w+:)?\/\/([^\s.]+\.\S{2}|localhost[:?\d]*)\S*$/;
-
-  if (pattern.test(input)) {
-    return true;
-  }
-  return pattern.test(`http://${input}`);
-};

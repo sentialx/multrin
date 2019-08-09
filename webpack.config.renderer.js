64391c18a571789c0a48d5a1250601a69f7a483f
Sential <sentialx@gmail.com>
2019-08-09 14:15:59 +0200

    chore: remove unnecessary dependencies, add webpack config

---
+++ webpack.config.renderer.js
@@ -0,0 +1,79 @@
+/* eslint-disable */
+const webpack = require('webpack');
+const { getConfig, dev } = require('./webpack.config.base');
+const { join } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+/* eslint-enable */
+
+const PORT = 4444;
+
+const getHtml = (scope, name) => {
+  return new HtmlWebpackPlugin({
+    title: 'Multrin',
+    template: 'static/pages/app.html',
+    filename: `${name}.html`,
+    chunks: [`vendor.${scope}`, name],
+  });
+};
+
+const applyEntries = (scope, config, entries) => {
+  for (const entry of entries) {
+    config.entry[entry] = [`./src/renderer/views/${entry}`];
+    config.plugins.push(getHtml(scope, entry));
+
+    if (dev) {
+      config.entry[entry].unshift('react-hot-loader/patch');
+    }
+  }
+};
+
+const getBaseConfig = name => {
+  const config = {
+    plugins: [new HardSourceWebpackPlugin()],
+
+    output: {},
+    entry: {},
+
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'initial',
+            name: `vendor.${name}`,
+            test: `vendor.${name}`,
+            enforce: true,
+          },
+        },
+      },
+    },
+  };
+
+  config.entry[`vendor.${name}`] = [
+    'react',
+    'react-dom',
+    'mobx',
+    'mobx-react-lite',
+    'styled-components',
+  ];
+
+  return config;
+};
+
+const appConfig = getConfig(getBaseConfig('app'), {
+  target: 'electron-renderer',
+
+  devServer: {
+    contentBase: join(__dirname, 'build'),
+    port: PORT,
+    hot: true,
+    inline: true,
+    disableHostCheck: true
+  },
+});
+
+applyEntries('app', appConfig, [
+  'app',
+]);
+
+module.exports = [appConfig, webConfig];
